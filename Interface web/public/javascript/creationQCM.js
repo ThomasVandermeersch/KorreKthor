424638f822922fa8a14dd25bba67d98c649fa601
ThomasVECAM <17030@ecam.be>
2021-02-03 20:16:32 +0100

    web interface to create MCQ

---
+++ Interface web/public/javascript/creationQCM.js
@@ -0,0 +1,90 @@
+
+var nbquestion = 5;
+const table = document.getElementById("table");
+const addQuestionButton = document.getElementById("addQuestion")
+
+
+
+function post(path, params, method='post') {
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    const form = document.createElement('form');
+    form.method = method;
+    form.action = path;
+  
+    for (const key in params) {
+      if (params.hasOwnProperty(key)) {
+        const hiddenField = document.createElement('input');
+        hiddenField.type = 'hidden';
+        hiddenField.name = key;
+        hiddenField.value = params[key];
+        form.appendChild(hiddenField);
+      }
+    }
+    document.body.appendChild(form);
+    form.submit();
+  }
+
+
+
+
+//Add a choice to a question
+document.querySelectorAll(".addChoice").forEach(item=>{
+    item.addEventListener("click",function(){
+        var row = table.rows[item.parentNode.parentNode.rowIndex]
+        row.insertCell(4).innerHTML +=  "<input type='checkbox' class='checkboxClass'  id='subscribeNews' name='subscribe' value='newsletter'>"
+    })
+})
+
+
+//Add a question
+addQuestionButton.addEventListener("click", function(){
+    nbquestion +=1  
+    var row = table.insertRow(nbquestion-1)
+    row.insertCell(0).innerHTML = "Question " + nbquestion
+    for(let i=1; i < 4; i++ ){
+        row.insertCell(i).innerHTML = "<input type='checkbox' class='checkboxClass'  id='subscribeNews' name='subscribe' value='newsletter'>"
+    }
+
+    var button = document.createElement("button")
+    button.type = "button"
+    button.innerHTML = "+"
+    row.insertCell(4).appendChild(button)
+
+    button.addEventListener("click",function(){
+        var row2 = table.rows[button.parentNode.parentNode.rowIndex]
+        var cell2 = row2.insertCell(4)
+        cell2.innerHTML +=  "<input type='checkbox' class='checkboxClass'  id='subscribeNews' name='subscribe' value='newsletter'>" 
+    })
+
+});
+
+
+//Send question to the server
+document.getElementById("send").addEventListener("click",function(){
+    console.log("hello world")
+    responseArr = []
+
+    for(let i=0; i<table.rows.length; i++){
+        responseArr.push([])
+    }
+    const test = []
+    document.querySelectorAll(".checkboxClass").forEach(item=>{
+
+        var row = item.parentNode.parentNode.rowIndex
+        var col = item.parentNode.cellIndex
+        responseArr[row][col -1] = item.checked
+        test.push(item.checked)
+    })
+
+    console.log(responseArr)
+    console.log(test)
+    post("/quest",{"liste":JSON.stringify(responseArr)})
+
+})
+
+//Remove last column
+document.getElementById("removeLast").addEventListener("click",function(){
+    table.deleteRow(table.rows.length -1);
+    nbquestion = nbquestion -1
+})
