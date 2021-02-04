5764b4f184fa9850ce3a6710708473a24958a430
ThomasVECAM <17030@ecam.be>
2021-02-04 14:53:31 +0100

    multiple MCQ

--- Interface web/public/javascript/creationQCM.js
+++ Interface web/public/javascript/creationQCM.js
@@ -1,9 +1,4 @@
 
-var nbquestion = 5;
-const table = document.getElementById("table");
-const addQuestionButton = document.getElementById("addQuestion")
-
-
 function post(path, params, method='post') {
     // The rest of this code assumes you are not using a library.
     // It can be made less wordy if you use one.
@@ -25,66 +20,72 @@ function post(path, params, method='post') {
     form.submit();
   }
 
-
-
-
 //Add a choice to a question
 document.querySelectorAll(".addChoice").forEach(item=>{
     item.addEventListener("click",function(){
+        var table = item.parentNode.parentNode.parentNode
         var row = table.rows[item.parentNode.parentNode.rowIndex]
         row.insertCell(4).innerHTML +=  "<input type='checkbox' class='checkboxClass'  id='subscribeNews' name='subscribe' value='newsletter'>"
     })
 })
 
+document.querySelectorAll(".addQuestion").forEach(item=>{
+    item.addEventListener("click",function(){
+        var table = item.parentNode.childNodes[1]
+        var nbRows = table.rows.length
+        var row = table.insertRow()
+        row.insertCell(0).innerHTML = "Question " + (nbRows + 1)
+        for(let i=1; i < 4; i++ ){
+            row.insertCell(i).innerHTML = "<input type='checkbox' class='checkboxClass'  id='subscribeNews' name='subscribe' value='newsletter'>"
+        }
 
-//Add a question
-addQuestionButton.addEventListener("click", function(event){
-    nbquestion +=1  
-    var row = table.insertRow(nbquestion-1)
-    row.insertCell(0).innerHTML = "Question " + nbquestion
-    for(let i=1; i < 4; i++ ){
-        row.insertCell(i).innerHTML = "<input type='checkbox' class='checkboxClass'  id='subscribeNews' name='subscribe' value='newsletter'>"
-    }
+        var button = document.createElement("button")
+        button.type = "button"
+        button.innerHTML = "+"
+        row.insertCell(4).appendChild(button)
 
-    var button = document.createElement("button")
-    button.type = "button"
-    button.innerHTML = "+"
-    row.insertCell(4).appendChild(button)
-
-    button.addEventListener("click",function(){
-        var row2 = table.rows[button.parentNode.parentNode.rowIndex]
-        var cell2 = row2.insertCell(4)
-        cell2.innerHTML +=  "<input type='checkbox' class='checkboxClass'  id='subscribeNews' name='subscribe' value='newsletter'>" 
+        button.addEventListener("click",function(){
+            var row2 = table.rows[button.parentNode.parentNode.rowIndex]
+            var cell2 = row2.insertCell(4)
+            cell2.innerHTML +=  "<input type='checkbox' class='checkboxClass'  id='subscribeNews' name='subscribe' value='newsletter'>" 
+        })
     })
+})
 
-});
+
+//Remove last column
+
+document.querySelectorAll(".removeQuestion").forEach(item=>{
+    item.addEventListener('click',function(){
+        var table =  item.parentNode.childNodes[1]
+        var lastRowIndex = table.rows.length  - 1
+        if ( lastRowIndex > 0){
+            table.deleteRow(lastRowIndex);
+        }
+    })
+})
+
 
 
 //Send question to the server
 document.getElementById("send").addEventListener("click", function(){
-    // console.log("hello world")
-    responseArr = []
-
-    for(let i=0; i<table.rows.length; i++){
-        responseArr.push([])
-    }
-    const test = []
-    document.querySelectorAll(".checkboxClass").forEach(item=>{
-
-        var row = item.parentNode.parentNode.rowIndex
-        var col = item.parentNode.cellIndex
-        responseArr[row][col -1] = item.checked
-        test.push(item.checked)
+    var responseObject = {}
+    document.getElementById("questions").childNodes.forEach(childNode=>{
+        //Prepare the response object
+        responseObject[childNode.id] = [[]] //add the first div
+        var table = childNode.childNodes[1]//get table of first div
+        for(let i=0; i<table.rows.length -1; i++){
+            responseObject[childNode.id].push([])
+        }
     })
-
-    post("/quest", {"liste":JSON.stringify(responseArr), "filename":JSON.stringify(document.getElementById("filename").innerText)})
-
-})
-
-//Remove last column
-document.getElementById("removeLast").addEventListener("click",function(){
-    if (nbquestion > 1){
-        table.deleteRow(table.rows.length -1);
-        nbquestion = nbquestion -1
-    }
+        //Fill in the responseObject
+        document.querySelectorAll(".checkboxClass").forEach(item=>{
+            //associate the item to a table.
+            var id = item.parentNode.parentNode.parentNode.parentNode.parentNode.id //lol
+            var row = item.parentNode.parentNode.rowIndex
+            var col = item.parentNode.cellIndex
+            responseObject[id][row][col -1] = item.checked
+        })
+        post("/quest", {"liste":JSON.stringify(responseObject), 
+                    "filename":document.getElementById("filename").innerTex})
 })
