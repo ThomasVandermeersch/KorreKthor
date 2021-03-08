733e9a0ddafe022886a2d2174b60b2dea10749d4
Guigxs <guigxs19@gmail.com>
2021-03-08 11:50:30 +0100

    remove log

--- Interface web/node_scripts/hasAcces.js
+++ Interface web/node_scripts/hasAcces.js
@@ -2,22 +2,17 @@ const { nextTick } = require("async")
 const user = require("./database/models/user")
 
 function hasAcces(req,res,next){
-    console.log(req.originalUrl)
     if(!req.session.userObject){
         return res.redirect("/auth/login")
     }
 
     var user = req.session.userObject
-    console.log(user)
     if (req.originalUrl.includes("/create") && user.role == 1){
         return next()
     }
     else{
-        // return next()
-
         return res.redirect("/fjne")
     }
-
 }
 
 exports.hasAcces = hasAcces
