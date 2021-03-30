53ed0955248116116238919fcea142e4f2aa9b96
Guigxs <guigxs19@gmail.com>
2021-03-31 00:21:56 +0200

    Fix the whole process

--- Traitment_images/main.py
+++ Traitment_images/main.py
@@ -4,8 +4,7 @@ import json
 from process_pdf import extractTextAndImg
 import make_pdf
 import process_img
-
-# pdf = make_pdf.makePdf('result', 'scan')
+import shutil
 
 def compute(pdf):
     jsonToSend = []
@@ -16,36 +15,55 @@ def compute(pdf):
         ImgDone = extractTextAndImg(pdf)
 
         if ImgDone == None :
-            jsonToSend.append({"error" : "No existing PDF"})
+            jsonToSend.append({"error" : f"{pdf} is not a PDF file"})
 
         else :
             print("\nGetting answers...")
-            # Zip folder From_PDF
             listPages = glob.glob('From_PDF/*.png')
 
             if len(listPages) == 0 :
-                jsonToSend.append({"error" : "No exported image from PDF"})
-            else :
-                for img in listPages :
+                jsonToSend.append({"error" : f"No image in {pdf}"})
+            else:
 
+                i = 0
+                firstQRCode = None
+                while firstQRCode == None:
+                    if i > 10:
+                        jsonToSend.append({"error" : f"No QRCode available in {pdf}"})
+                        break
+
+                    firstQRCode = process_img.decodeQRCode(listPages[i])
+                    i+=1
+
+                for img in listPages :
                     answers = process_img.process(img)
 
                     if answers == None:
                         jsonToSend.append({"error" : f"{img} is not a QCM file"})
                     elif answers == False :
-                        jsonToSend.append({"error" : f"{img}, no answers scanned"})
-                    else :
-                        qrcode = process_img.decodeQRCode(img)
+                        jsonToSend.append({"error" : f"No answers scanned in {img}"})
+                    else:
 
-                        if qrcode and "version" in qrcode and "matricule" in qrcode and "lessonId" in qrcode:
-                            jsonToSend.append({"qrcode":qrcode, "answers":answers, "file":img, "error" : "None"})
-                        else:
+                        qrcode = process_img.decodeQRCode(img)
+                        if firstQRCode["lessonId"] != qrcode["lessonId"]:
+                            jsonToSend.append({"error" : f"{img} does not belong to the lesson : {firstQRCode}"})
+
+                        if not qrcode or "version" not in qrcode or "matricule" not in qrcode or "lessonId" not in qrcode:
                             jsonToSend.append({"error" : f"{img} has no correct QR Code"})
+                            
+                        else:
+                            jsonToSend.append({"qrcode":qrcode, "answers":answers, "file":img.split("/")[-1], "error" : "None"})
                         
 
-    # 
-    print(json.dumps(jsonToSend))
-    print("\nTranslation done!\n")
-    return json.dumps(jsonToSend)
+    # Zip folder in order to send it
+    zipPath = f'zips/{firstQRCode["lessonId"]}'
+    try:
+        shutil.make_archive(zipPath, "zip", "From_PDF")
+        response = {"zipFile":f"{firstQRCode['lessonId']}.zip", "data":jsonToSend}
+        print(f"\nTransaction done, file in : {zipPath}.zip !\n")
+    except:
+        print(f"Error while zipping to {zipPath}")
+        response = {"error":f"Zipping to {zipPath} failed"}
 
-# compute("scan_result.pdf")
+    shutil.rmtree("From_PDF/")
+    return response
