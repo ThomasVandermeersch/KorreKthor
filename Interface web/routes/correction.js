55b1612db808969a4945b2f9c0a85c66a906f8f3
ThomasVandermeersch <17030@ecam.be>
2021-05-03 22:44:14 +0200

    update correction js

--- Interface web/routes/correction.js
+++ Interface web/routes/correction.js
@@ -6,41 +6,53 @@ const sendEmail = require('../node_scripts/sendEmail');
 const path = require("path")
 const functions = require("../node_scripts/functions")
 
-router.get("/modifyCriteria/:examId", access.hasAccess, async (req,res)=>{
-    var exam = await Exam.findOne({where:{id:req.params.examId}})
+router.get("/modifyCriteria/:examId", access.hasAccess,(req,res)=>{
+    if (req.session.userObject.authorizations == 3 && req.session.userObject.role == 0) return res.redirect("/noAccess")
 
-    var correctionCriterias = JSON.parse(exam.correctionCriterias)
-    correctionCriterias['redirection'] = 'modify'
-    req.session['examId'] = req.params.examId        
-    res.render('correction/modifyCriteria.pug', correctionCriterias)
+    const userMatricule = req.session.userObject.matricule
+    var query;
+
+    if (req.session.userObject.authorizations == 0) query = {where:{id:req.params.examid}}
+    else query = {where:{userMatricule:userMatricule, id:req.params.examid}}
+
+    Exam.findOne(query).then(exam=>{
+        if(exam){
+            var correctionCriterias = JSON.parse(exam.correctionCriterias)
+            correctionCriterias['redirection'] = 'modify'
+            req.session['examId'] = req.params.examId        
+            return res.render('correction/modifyCriteria.pug', correctionCriterias)
+        }
+        console.log(" --- EXAM DOES NOT EXIST ERROR -- correction/modifyCriteria/examId ---\n ")
+        req.flash('errormsg', 'This exam does not exist, error : 1019')
+        return res.redirect("/error")
+
+    }).catch(err=>{
+        console.log(" --- DATABASE ERROR -- correction/modifyCriteria/:examId ---\n " + err)
+        req.flash('errormsg', 'Database error, error : 1020')
+        return res.redirect('/error')
+    })
 })
 
-router.get("/questionStatus/:examId", access.hasAccess, async (req,res)=>{
-    const exam = await Exam.findOne({where:{id:req.params.examId}})
-    const questionStatus = JSON.parse(exam.questionStatus)
-    res.render('correction/questionStatus',{questionStatus:questionStatus, exam:exam})
-})
+router.get("/questionStatus/:examId", access.hasAccess,(req,res)=>{
+    if (req.session.userObject.authorizations == 3 && req.session.userObject.role == 0) return res.redirect("/noAccess")
 
-router.post('/modifyQuestionStatus/:examId',async(req,res)=>{    
-    var questionStatus = JSON.parse(req.body.questionStatusObject)
-    var newStatus = req.body.type
-    var index = 0
-    Object.entries(questionStatus).forEach(([key,value]) =>{
-        slice = newStatus.slice(index, index + value.length);
-        index += value.length
-        questionStatus[key] = slice
-    })
+    const userMatricule = req.session.userObject.matricule
+    var query;
 
-    var exam = await Exam.findOne({where:{id:req.params.examId}})
-    exam.questionStatus = JSON.stringify(questionStatus)
-    await exam.save()
+    if (req.session.userObject.authorizations == 0) query = {where:{id:req.params.examid}}
+    else query = {where:{userMatricule:userMatricule, id:req.params.examid}}
 
-    corrector.reCorrect(req.params.examId).then(suc=>{
-        res.redirect(`/see/exam/${req.params.examId}`)
-    })
-    .catch(err=>{
-        console.log(err)
-        res.end('Problem')
+    Exam.findOne(query).then(exam=>{
+        if(exam) return res.render('correction/questionStatus',{questionStatus:JSON.parse(exam.questionStatus), exam:exam})
+
+        console.log(" --- EXAM DOES NOT EXIST ERROR -- correction/questionStatus/:examId ---\n ")
+        req.flash('errormsg', 'This exam does not exist, error : 1019')
+        return res.redirect("/error")
+
+    }).catch(err=>{
+        console.log(" --- DATABASE ERROR -- correction/questionStatus/:examId ---\n " + err)
+        req.flash('errormsg', 'Database error, error : 1020')
+        return res.redirect('/error')
     })
 })
 
@@ -73,17 +85,90 @@ router.get('/sendEmail/:copyid',access.hasAccess, async(req,res)=>{
     }
 })
 
+router.get("/downloadExcel/:examId", access.hasAccess, async (req,res)=>{
+    const exam = await Exam.findOne({where:{id:req.params.examId}, attributes:["excelFile"], include:[{model:Copy, as:"copies", attributes:["result", "version"], include:[{model:User, as:"user", attributes:["matricule", "fullName", "role"]}]}]})
+    const excelFilePath =  exam.excelFile
+
+    data = {}
+    exam.copies.forEach((copy) => {
+        data[copy.user.matricule] = copy
+    })
+    
+    err = await functions.exportStudents({"name":exam.name, "excelFile":exam.excelFile, "id":exam.id}, data)
+
+    if (err){
+        req.flash("errormsg", err)
+        return res.status(500).render("index/error")
+    }
+
+    res.download(
+        path.resolve(excelFilePath),
+        (err) => {
+            if (err) res.status(404).send("<h1>File Not found: 404</h1>");
+        }
+    );
+})
+
+
+router.post('/modifyQuestionStatus/:examId',async(req,res)=>{ 
+    if (req.session.userObject.authorizations == 3 && req.session.userObject.role == 0) return res.redirect("/noAccess")
+
+    const userMatricule = req.session.userObject.matricule
+    var query;
+
+    if (req.session.userObject.authorizations == 0) query = {where:{id:req.params.examid}}
+    else query = {where:{userMatricule:userMatricule, id:req.params.examid}}
+    
+    Exam.findOne(query).then(exam=>{
+        if(!exam){
+            console.log(" --- EXAM DOES NOT EXIST ERROR -- POST correction/modifyQuestionStatus/examId ---\n ")
+            req.flash('errormsg', 'This exam does not exist, error : 1019')
+            return res.redirect("/error")
+        }
+        var questionStatus = JSON.parse(req.body.questionStatusObject)
+        var newStatus = req.body.type
+        var index = 0
+        Object.entries(questionStatus).forEach(([key,value]) =>{
+            slice = newStatus.slice(index, index + value.length);
+            index += value.length
+            questionStatus[key] = slice
+        })
+        exam.questionStatus = JSON.stringify(questionStatus)
+        exam.save().then(exam=>{
+            corrector.reCorrect(req.params.examId).then(suc=>{
+                req.flash('recorrectmsg', 'Question status saved and exam recorrect successfuly')
+                return res.redirect(`/see/exam/${req.params.examId}`)
+            })
+            .catch(err=>{
+                console.log(" ---  ERROR WHILE RECORECT EXAM -- POST correction/modifyQuestionStatus/examId ---\n " +err)
+                req.flash('errormsg', 'Error while re-correct exam, error : 1019')
+                return res.redirect("/error")
+            })
+        }).catch(err=>{
+            console.log(" ---  ERROR WHILE SAVING EXAM -- POST correction/modifyQuestionStatus/examId ---\n " + err)
+            req.flash('errormsg', 'Error while saving exam, error : 1019')
+            return res.redirect("/error")
+        })
+    }).cath(err=>{
+        console.log(" --- DATABASE ERROR -- correction/questionStatus/:examId ---\n " + err)
+        req.flash('errormsg', 'Database error, error : 1020')
+        return res.redirect('/error')
+    })
+})
+
+
+
 router.post('/modifyImageTreatment/:copyId', access.hasAccess, async (req,res)=>{
-    console.log(req.body)
+    
     var copy = await Copy.findOne({where:{id:req.params.copyId}})
     const exam = await Exam.findOne({where:{id:copy.examId}})
     copy.answers = req.body.response
     
-    const corrections = JSON.parse(exam.corrections)
-    const correction = corrections[copy.version]
-    const correctionCriterias = JSON.parse(exam.correctionCriterias)
-    var questionStatus = JSON.parse(exam.questionStatus)
-    questionStatus = questionStatus[copy.version]
+    const corrections = JSON.parse(exam.corrections) //GET corrections
+    const correction = corrections[copy.version] //Get correction of the version
+    const correctionCriterias = JSON.parse(exam.correctionCriterias) //Get correction criterias
+    var questionStatus = JSON.parse(exam.questionStatus) //Get question status
+    questionStatus = questionStatus[copy.version] //Get question status of the version
 
     corrector.correctionCopy( 
         correction,JSON.parse(req.body.response),questionStatus,correctionCriterias           
@@ -113,30 +198,5 @@ router.post('/sendComplainEmail',access.hasAccess,(req,res)=>{
         res.redirect('/correction/sendEmail/'+ req.body.copyId)
     })
 })
-
-router.get("/downloadExcel/:examId", access.hasAccess, async (req,res)=>{
-    const exam = await Exam.findOne({where:{id:req.params.examId}, attributes:["excelFile"], include:[{model:Copy, as:"copies", attributes:["result", "version"], include:[{model:User, as:"user", attributes:["matricule", "fullName", "role"]}]}]})
-    const excelFilePath =  exam.excelFile
-
-    data = {}
-    exam.copies.forEach((copy) => {
-        data[copy.user.matricule] = copy
-    })
-    
-    err = await functions.exportStudents({"name":exam.name, "excelFile":exam.excelFile, "id":exam.id}, data)
-
-    if (err){
-        req.flash("errormsg", err)
-        return res.status(500).render("index/error")
-    }
-
-    res.download(
-        path.resolve(excelFilePath),
-        (err) => {
-            if (err) res.status(404).send("<h1>File Not found: 404</h1>");
-        }
-    );
-})
-
 
 module.exports = router
